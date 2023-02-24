9c7515f03a718f62a533ff2838ca6918a98d47a6
tombuildsstuff <git@tom.ibuildstuff.eu>
2023-02-24 12:59:23 +0100

    updating the script to account for darwin sed being different

--- scripts/update-user-agent.sh
+++ scripts/update-user-agent.sh
@@ -3,4 +3,10 @@
 # Since this SDK is no longer present within `Azure/azure-sdk-for-go`, using the user agent/version number from that
 # is misleading - it also means we unintentionally import the Azure SDK for Go.
 # Instead let's update the user agent to use a local version with the version number at release time
-find ../../sdk -type f -print0 | xargs -0 sed -i '' -e "s/Azure\/azure-sdk-for-go\/version/tombuildsstuff\/kermit\/version/" -e "s/Azure-SDK-For-Go\//tombuildsstuff\/kermit\//"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  find ../../sdk -type f -print0 | xargs -0 sed -i '' -e "s/Azure\/azure-sdk-for-go\/version/tombuildsstuff\/kermit\/version/" -e "s/Azure-SDK-For-Go\//tombuildsstuff\/kermit\//"
+else
+  find ../../sdk -type f -print0 | xargs -0 sed -i -e "s/Azure\/azure-sdk-for-go\/version/tombuildsstuff\/kermit\/version/" -e "s/Azure-SDK-For-Go\//tombuildsstuff\/kermit\//"
+fi
+
